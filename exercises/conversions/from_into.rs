24fe87bd9c4eb3270b14036c75fddfaef0cc0bff
Ryan Clements <ryanclementshax@gmail.com>
2023-06-15 01:02:09 +0000

    finished clippy, conversions, iterators, macros, smart_pointers, and threads

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,26 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Default::default()
+        }
+        let split: Vec<_> = s.split(',').collect();
+        if split.len() != 2 {
+            return Default::default()
+        }
+        let name = split[0];
+        if name.len() == 0 {
+            return Default::default()
+        }
+        if let Ok(parsed_age) = split[1].parse::<usize>() {
+            return Person {
+                name: name.to_string(),
+                age: parsed_age
+            }
+        }
+        Default::default()
     }
 }
 
